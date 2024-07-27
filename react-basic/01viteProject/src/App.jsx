df04d5058489f6650982208b513ef14a9eb23cad
KaiLegend56 <kaininja567@gmail.com>
2024-07-27 22:46:39 +0530

    custom react created

--- react-basic/01viteProject/src/App.jsx
+++ react-basic/01viteProject/src/App.jsx
@@ -1,12 +1,13 @@
 import { Ww2 } from "./Chai.jsx"
 
-function App() {
- 
+function App() { 
+  const username = "we will miss you fuhrer"
 
   return (
     <>
+
     <Ww2/>
-    <h1>lol boll</h1>
+    <h1>lol boll {username}</h1> // this is called evaluated expression (no if or for, it should be finished product)
     </>
   ) // only single element needs to be exported wrap them under fragment
 }
--- react-basic/01viteProject/src/main.jsx
+++ react-basic/01viteProject/src/main.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){ // without using the App
+  return (
+    <div>
+      <h1>kill you</h1>
+      </div>
+  )
+}
+
+// const ReactElement = 
+//   {
+//     type:'a',
+//     props:{
+//         href:"https://google.com",
+//         target:"_self"
+//     },
+//     children:"click me to go to google"
+// } // react element object (our own custom)
+
+const anotherElement = (
+  <a href="https://google.com" tagret="_blank">visit google</a>
+)
+
+const reactElement = React.createElement("a",{ 
+  href:"https://google.com",
+  target:"_blank"
+  },
+  "visit google after clicking",
+  anotherElement // evaluated expression where variables are injected
+) // babble injects this code
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ //<App/>
+   reactElement // this will work 
+ //<ReactElement/> // wont work because react .render() accepts object in diffrent order when it is converted into a tree
+ // anotherElement this will work
+   // <MyApp/>  
+  // MyApp() my app can also be used this way
 )
