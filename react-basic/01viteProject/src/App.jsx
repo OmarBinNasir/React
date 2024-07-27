8fff179283c50f8069ebb6f52830664b3df6dfff
KaiLegend56 <kaininja567@gmail.com>
2024-07-27 10:51:26 +0530

    basic react

---
+++ react-basic/01viteProject/src/App.jsx
@@ -0,0 +1,14 @@
+import { Ww2 } from "./Chai.jsx"
+
+function App() {
+ 
+
+  return (
+    <>
+    <Ww2/>
+    <h1>lol boll</h1>
+    </>
+  ) // only single element needs to be exported wrap them under fragment
+}
+
+export default App
---
+++ react-basic/01viteProject/src/Chai.jsx
@@ -0,0 +1,5 @@
+export function Ww2(){
+    return (
+        <h1>we lost sire.</h1>
+    )
+}
