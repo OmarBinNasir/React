beb94c13edb5117b3c0fab46d87bf8e9752062c7
KaiLegend56 <kaininja567@gmail.com>
2024-07-28 17:41:23 +0530

    useState explained

---
+++ react-basic/02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter = 5 this variable wont display on the ui
+  let [counter,setCounter]=useState(15)
+
+  const addValue = () => {
+    if(!(counter===20))
+    counter++
+    
+    setCounter(counter)
+    //console.log(counter)
+  }
+  const removeValue = ()=>{
+    if(!(counter===0))
+    counter--;
+    
+    setCounter(counter)
+  }
+  return (
+    <>
+      <h1>japan emperor died </h1>
+      <h2>counter : {counter} </h2>
+      <button
+        onClick={addValue}
+      >add the count</button>
+      <button
+      onClick={removeValue}>delete the count</button>
+    </>
+  )
+}
+
+export default App
