24897d27911e9670daf184e2b9e26065f9eb8f7d
KaiLegend56 <kaininja567@gmail.com>
2024-08-12 08:47:57 +0530

    added .env variables for appwrite set up

--- react-basic/12MegaProject/Haard-way/src/App.jsx
+++ react-basic/12MegaProject/Haard-way/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-
+console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>
-      
+      <h1>welcome to gaylamd</h1>
     </>
   )
 }
