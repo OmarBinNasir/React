5bf95e5df761786454a8264ffb8ab9d562dfb226
KaiLegend56 <kaininja567@gmail.com>
2024-08-12 19:41:04 +0530

    appwrite authentication

---
+++ react-basic/12MegaProject/Haard-way/src/appwrite/Auth.js
@@ -0,0 +1,54 @@
+import conf from "../conf/conf";
+import {Client, Account, ID } from "appwrite"
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    contructor(){
+        this.client
+                .setEndpoint(conf.appwriteUrl)
+                .setProject(conf.appwriteProjectId);
+        this.account = new Account( this.client );        
+    }
+
+    async createAccount({ email, password, name}) {
+        try {
+          const userAccount = await this.account.create( ID.unique(), email, password, name );
+          if(userAccount){
+            return await this.login({ email, password })
+          }
+          else{ 
+          return userAccount;
+          }
+        } catch (error) {
+            console.log(" user account is not created",error)
+        }
+    }
+    async login({ email, password }){
+        try{
+           return await this.account.createEmailSession( email, password )
+        }
+        catch(error){
+            console.log("invalid credentials ",error)
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return this.account.get();
+           
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+        return null;
+    } 
+    async logoutUser () {
+        try {
+            this.account.deleteSessions()
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export const authService = new AuthService();
