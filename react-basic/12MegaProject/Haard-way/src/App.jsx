6277dbfb9ff00a07b7694dcb32a8634aafed61a8
KaiLegend56 <kaininja567@gmail.com>
2024-08-13 17:44:57 +0530

    loading state using authService, provider to the main

--- react-basic/12MegaProject/Haard-way/src/App.jsx
+++ react-basic/12MegaProject/Haard-way/src/App.jsx
@@ -1,14 +1,41 @@
-
+import React,{ useState, useEffect } from 'react'
+import { authService } from './appwrite/Auth'
+import { login, logout} from "./store/authSlice"
+import { useDispatch } from "react-redux"
 import './App.css'
+import { Header, Footer } from './components'
+import { Outlet } from "react-router-dom"
+
+
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL)
 
-  return (
-    <>
-      <h1>welcome to gaylamd</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(false)  // when app loads, checks if user is logged in or not
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData)
+       dispatch(login({ userData }))
+      else
+        dispatch(logout())
+    })
+    .finally(() => setLoading(false))
+  })
+
+  return !loading ?
+   <div className=" min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="">
+        <Header />
+        <main>
+      TODO:  { /* <Outlet /> */ }
+        </main>
+       
+        <Footer />
+    </div>
+  </div> : null
 }
 
 export default App
