1dffd04abbe47c1ecbef6d519e1bf1a1f0e426e2
KaiLegend56 <kaininja567@gmail.com>
2024-08-17 16:48:17 +0530

    PostForm created with submit functionality

--- react-basic/12MegaProject/Haard-way/src/components/index.js
+++ react-basic/12MegaProject/Haard-way/src/components/index.js
@@ -8,6 +8,7 @@ import Button from "./Button"
 import Input from "./Input"
 import Select from "./Select"
 import Login from "./Login";
+import RTE from "./RTE";
 
     
-export { Header, Footer, Container, LogoutBtn, Logo, PostCard, Button, Input, Select, Login} 
+export { Header, Footer, Container, LogoutBtn, Logo, PostCard, Button, Input, Select, Login, RTE, } 
